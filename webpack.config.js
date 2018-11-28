4f054a7f6466adb2d0fa0a915959c1c33c187624
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2018-11-27 21:30:55 -0600

    Initial Commit For React App

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    BMS_React_Hello_World: './builds/BMS_React_Hello_World.js'
+    //BMS_some_Other_module: './builds/BMS_some_Other_module.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../assets')
+  }
+};
